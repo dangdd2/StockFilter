4ad22afebd8a02e93f8ad6e384a6acb14c093569
dinh dang <dang.dang@episerver.com>
2020-01-31 16:22:38 +0700

    initialize project

---
+++ src/StockFilter/StockFilter/FireAnt/FireAntClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Deserializers;
+
+namespace StockFilter
+{
+    public class FireAntClient : BaseClient
+    {
+
+        private static string _baseUrl = "https://svr3.fireant.vn";
+        private static string api = "/api/Data/Finance/LastestFinancialInfo";
+        public FireAntClient(ICacheService cache, IDeserializer serializer, IErrorLogger errorLogger, string baseUrl) : base(cache, serializer, errorLogger, baseUrl)
+        {
+        }
+
+        public FireAntClient(IDeserializer serializer, IErrorLogger errorLogger) : base(serializer, errorLogger, _baseUrl)
+        {
+        }
+
+        public StockItem GetStockInfo(string symbol)
+        {
+            var request = new RestRequest(api, Method.GET);
+            request.AddQueryParameter("symbol", symbol);
+
+            return Get<StockItem>(request);
+        }
+
+    }
+}
---
+++ src/StockFilter/StockFilter/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp.Serialization.Json;
+
+namespace StockFilter
+{
+    class Program
+    {
+        #region MyRegion
+        static List<string> vn30 = new List<string>()
+        {
+            "BID",
+            "BVH",
+            "CTD",
+            "CTG",
+            "DPM",
+            "EIB",
+            "FPT",
+            "GAS",
+            "GMD",
+            "HDB",
+            "HPG",
+            "MBB",
+            "MSN",
+            "MWG",
+            "NVL",
+            "PNJ",
+            "REE",
+            "ROS",
+            "SAB",
+            "SBT",
+            "SSI",
+            "STB",
+            "TCB",
+            "VCB",
+            "VHM",
+            "VIC",
+            "VJC",
+            "VNM",
+            "VPB",
+            "VRE"
+        };
+
+        static List<string> others = new List<string>()
+        {
+            "POW",
+            "BSR"
+        };
+        #endregion
+        static void Main(string[] args)
+        {
+            var cache = new JsonSerializer();
+            var fireAntClient = new FireAntClient(cache,new ErrorLogger());
+            var data = new List<StockItem>();
+
+            
+            var stockList = vn30.Union(others).Distinct();
+
+            foreach (var symbol in stockList)
+            {
+                data.Add(fireAntClient.GetStockInfo(symbol));
+            }
+            
+            
+            ImportToSQLServer(data);
+
+            Console.WriteLine(" -- DONE -- ");
+            Console.ReadLine();
+        }
+
+        private static void ImportToSQLServer(List<StockItem> data)
+        {
+            var type = Type.GetType("StockFilter.StockItem");
+
+            var table = new Table(type, "");
+            table.Create();
+
+            table.Insert(data);
+        }
+    }
+}
